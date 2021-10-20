260d93049b9482790881f09f718f0342dc625df8
3v <ventic@3v.fi>
2021-10-20 05:10:28 +0200

    fix dev deps

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM node:lts-alpine
+FROM node:alpine
 WORKDIR /app
 COPY . .
 RUN npm i
 RUN npm run build
 
-FROM node:lts-alpine
+FROM node:alpine
 WORKDIR /app
 COPY --from=0 /app .
 RUN npm ci
--- rollup.config.js
+++ rollup.config.js
@@ -19,7 +19,7 @@ const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 const onwarn = (warning, onwarn) =>
   (warning.code === "MISSING_EXPORT" && /'preload'/.test(warning.message)) ||
   (warning.code === "CIRCULAR_DEPENDENCY" &&
-    /[/\\]@sapper[/\\]/.test(warning.message)) ||
+    /[/\\](@sapper|@carbon|d3-\w+)[/\\]/.test(warning.message)) ||
   warning.code === "THIS_IS_UNDEFINED" ||
   onwarn(warning);
 
